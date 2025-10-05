7b7ce2cded016c0145710c74f765f10ae4edde0d
Eclipsed <unity.homeless.kingdom@gmail.com>
2025-10-05 21:05:49 +0300

    Best Healer Founder script was added, but need to be improved by adding AP(Ability point)s and upgrades that require level and etc.

--- Program.cs
+++ Program.cs
@@ -1,97 +1,156 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 
 namespace DeadLockHelper
 {
-    class Program
+    class Person
     {
-        class Person
+        public Person()
         {
-            public Person()
+            //double dps = DamagePerSecond();
+            Console.WriteLine(
+            $"Длительность отхила от Динамо = {DdurationOfHealthRegen}" +
+            $"\nДлительность отхила от Вязкуса = {VdurationOfHealthRegen}" +
+            $"\nКД отхила от Вязкуса = {VcooldownOfCube}" +
+            $"\nКД отхила от Динамо = {DcooldownOfAurora}" +
+            $"\nБазовое хп бибопа = {bibopBaseHealth}");
+            double DB = 0, VB = 0, MB = 0, NNB = 0;
+            int[] DBLevels = new int[maxLevel], VBLevels = new int[maxLevel], MBLevels = new int[maxLevel], NNBLevels = new int[maxLevel];
+            string totalDBLevels = "", totalVBLevels = "", totalMBLevels = "", totalNNBLevels = "";
+            int DBindex = 0, VBindex = 0, MBindex = 0, NNBindex = 0;
+            int DBStreak = 0, VBStreak = 0, MBStreak = 0, NNBStreak = 0;
+            while (currentLevel <= maxLevel)
             {
-                double btk = BulletsToKill();
-                double ttk = TimeToKill();
-                double mtk = MagazinesToKill();
-                double dps = DamagePerSecond();
-
+                if (DbaseHealthRegen > VbaseHealthRegen && DbaseHealthRegen > MbaseHealthRegen)
+                {
+                    if (DBStreak == 0) { DB++; DBLevels[DBindex] = currentLevel; totalDBLevels += DBLevels[DBindex] + " "; DBindex++; }
+                    else if (DBStreak > 0) { DB++; DBLevels[DBindex] = currentLevel; totalDBLevels += DBLevels[DBindex] + " "; DBindex++; }
+                }
+                else if (VbaseHealthRegen > DbaseHealthRegen && VbaseHealthRegen > MbaseHealthRegen) { VB++; VBLevels[VBindex] = currentLevel; totalVBLevels += VBLevels[VBindex] + " "; VBindex++; }
+                else if (MbaseHealthRegen > DbaseHealthRegen && MbaseHealthRegen > VbaseHealthRegen) { MB++; MBLevels[MBindex] = currentLevel; totalMBLevels += MBLevels[MBindex] + " "; MBindex++; }
+                else if (MbaseHealthRegen == DbaseHealthRegen && MbaseHealthRegen == VbaseHealthRegen) { NNB++; NNBLevels[NNBindex] = currentLevel; totalNNBLevels += MBLevels[NNBindex] + " "; NNBindex++; }
                 Console.WriteLine(
-                $"Пуль для убийства: {btk} "
-                + $"\nВремя для убийства: {ttk}с."
-                + $"\nОбойм для убийств: {mtk}"
-                + $"\nУрон в секунду: {dps}");
+                        $"\nТекущий уровень = {currentLevel}" +
+                        $"\nСпиритическая мощь Динамо = {DspiritPower}" +
+                        $"\nСпиритечкая мощь Вязкуса = {VspiritPower}" +
+                        $"\nОтхил от Динамо = {DbaseHealthRegen}" +
+                        $"\nОтхил от Вязкуса = {VbaseHealthRegen}\n");
+                currentLevel++;
             }
-            public double BulletsToKill()
+            string finalString = "";
+            if (totalVBLevels != "") { finalString += $"Лвла когда Вязкус хиляет лучше всех: {totalVBLevels}\n"; }
+            if(totalDBLevels != "") { finalString += $"Лвла когда Динамо хиляет лучше всех: {totalDBLevels}\n"; }
+            if(totalMBLevels != "") { finalString += $"Лвла когда Макгиннис хиляет лучше всех: {totalMBLevels}\n"; }
+            if(totalNNBLevels != "") { finalString += $"Лвла когда все одинаково хиляют: {totalNNBLevels}\n"; }
+            Console.WriteLine(finalString);
+
+            /*Console.WriteLine(
+            $"Пуль для убийства: {BulletsToKill} "
+            + $"\nВремя для убийства: {ttk}с."
+            + $"\nОбойм для убийств: {MagazinesToKill}"
+            + $"\nУрон в секунду: {dps}");*/
+        }
+        public double DamagePerSecond()
+        {
+            double dps = 0;
+            double remainingTime = 1;
+            while (remainingTime > 0)
             {
-                double btk = Math.Ceiling(Health / Damage);
-                return btk;
-            }
-            public double MagazinesToKill()
-            {
-                double mtk = BulletsToKill() / Ammo;
-                return mtk;
-            }
-            public double DamagePerSecond()
-            {
-                double dps = 0;
-                double remainingTime = 1;
                 if (BulletsPerSecond > Ammo && ReloadTime < 1 && remainingTime - ReloadTime - (TimeForOneBullet * (Ammo + 1)) >= 0)
                 {
+                    Console.WriteLine(3);
                     double BulletsToShoot = 0;
-                    while (remainingTime > 0)
+                    remainingTime -= TimeForOneBullet * Ammo + ReloadTime;
+                    BulletsToShoot += Ammo;
+                    double BulletsInRemTime = Math.Floor(remainingTime / TimeForOneBullet);
+                    if (BulletsInRemTime >= 1 && BulletsInRemTime <= Ammo)
                     {
-                        if ((remainingTime - TimeForOneBullet * Ammo) >= 0)
-                        {
-                            remainingTime -= TimeForOneBullet * Ammo;
-                            BulletsToShoot += Ammo;
-                            if ((remainingTime - ReloadTime) >= 0)
-                            {
-                                remainingTime -= ReloadTime;
-                                BulletsToShoot += Math.Floor(remainingTime / TimeForOneBullet);
-                            }
-                            dps += BulletsToShoot * Damage;
-                        }
-                        else if ((remainingTime - TimeForOneBullet) >= 0)
-                        {
-                            dps += Math.Floor(remainingTime / TimeForOneBullet) * Damage;
-                        }
+                        Console.WriteLine(6);
+                        BulletsToShoot += BulletsInRemTime;
                     }
-                    //remainingTime = remainingTime - ReloadTime - (TimeForOneBullet * Ammo);
-                    //double a = (1 - ReloadTime) / BulletsPerSecond;
-                    //Console.WriteLine(a);
-                    //dps = (Damage * BulletsPerSecond * Ammo) / (Ammo / BulletsPerSecond + ReloadTime);
-                    //3 * 3.14 * 5 / (5 / 3.14 + 2.59) = 11.26
+                    else if (BulletsInRemTime > Ammo)
+                    {
+                        Console.WriteLine(-6);
+                        BulletsToShoot += Math.Floor(TimeForOneBullet * Ammo / remainingTime);
+                    }
+                    dps += BulletsToShoot * Damage;
                 }
                 else
                 {
-                    dps = Damage * Math.Min(Math.Floor(BulletsPerSecond), Ammo);
+                    Console.WriteLine(-3);
+                    dps += Damage * Math.Min(Math.Floor(BulletsPerSecond), Ammo);
+                    break;
                 }
-                return dps;
             }
-            public double TimeToKill()
-            {
-                //Нужно учесть перезарядку,
-                //восстановление хп, урон,
-                double ttk = Health / (Damage * BulletsPerSecond);
-                return ttk;
-            }
-            //stats
-            //TimeToShootFullMagazine
-            public double ttsfm => Ammo / BulletsPerSecond;
-            public double TimeForOneBullet => 1 / BulletsPerSecond;
-            public double Health = 650;
-            public double HealthRegen = 2;
-            public double Damage = 5;
-            public double BulletsPerSecond = 4;
-            public double Ammo = 5.0;
-            public double BulletSpeed = 254;
-            public double ReloadTime = 2.59;
-            public double Distance = 15;
+            return dps;
         }
+        public double TestingTimeToKill()
+        {
+            //восстановление хп и т.д.
+            //Учесть время на выстрел патрон, после чего добавить хп которое добавилось бы за этот промежуток времени
+            double HeroHealth = Health;
+            double nrttk = 0;
+            double remainingHp = 0;
+            //Ещё не готово
+            while (HeroHealth > 0)
+            {
+                remainingHp = HeroHealth / (Damage * Ammo);
+                if (remainingHp > 0)
+                {
+                    HeroHealth -= Damage * Ammo;
+                    nrttk += TimeForOneBullet * Ammo;
+                    if (HeroHealth > 0) nrttk += ReloadTime;
+                }
+                else if (remainingHp < 0)
+                {
+                    HeroHealth = HeroHealth / Damage;
+                }
+            }
+            double ttk = BulletsToKill * TimeForOneBullet + MagazinesToKill * ReloadTime;
+            return ttk;
+        }
+        //stats
+        //TimeToShootFullMagazine = ttsfm
+        public double ttk => BulletsToKill * TimeForOneBullet + MagazinesToKill * ReloadTime;
+        public double BulletsToKill => Math.Ceiling(Health / Damage);
+        public double MagazinesToKill => Math.Ceiling(BulletsToKill / Ammo);
+        public double ttsfm => Ammo / BulletsPerSecond;
+        public double TimeForOneBullet => 1 / BulletsPerSecond;
+        public double Health = 650;
+        public double HealthRegen = 2;
+        public double Damage = 5;
+        public double BulletsPerSecond = 5;
+        public double Ammo = 5.0;
+        public double BulletSpeed = 254;
+        public double ReloadTime = 0.1;
+        public double Distance = 15;
+
+        public double MbaseHealthRegen = 25;
+        public double McooldownOfSpecter = 48;//48s
+
+        public double VbaseHealthRegen => 30 + 0.14 * VspiritPower;
+        // 55 will be with AP = 3
+        public double VcooldownOfCube = 42; //21s
+        public double VdurationOfHealthRegen = 4;//4s
+        public double VspiritPower => 1.1 * currentLevel;
+
+        public double DcooldownOfAurora = 48; //34s
+        public double DdurationOfHealthRegen = 5;//4s
+        public double DbaseHealthRegen => 25 + 0.4 * DspiritPower;
+        public double DspiritPower => 1.1 * currentLevel;
+
+        public double bibopBaseHealth => 800 + 54 * currentLevel;
+
+        public int currentLevel = 1;
+        public int maxLevel = 34; // bcs lvl starts at 0 in game, but i dont want to 
+        // total AP = 30
+        // AP to max one skill = 8
+        public double AP = 0; // 1, 3, 5, 7 -> 33
+        // not gaining AP on levels 0 | 2 | 4 | 6
         static void Main()
         {
-            //Console.WriteLine(15 % 1 == 0);
             Person Adam = new Person();
             Console.WriteLine($"Хп: {Adam.Health}");
-            //Console.WriteLine("Hello World!");
         }
     }
 }
